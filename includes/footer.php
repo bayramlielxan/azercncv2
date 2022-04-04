41f49bb5b2dd59c20ae0d509db1d27faa4abe91d
Elxan <testforlove@mail.ru>
2022-04-04 13:01:36 +0400

    includes

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+<footer  class="site-footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 col-sm-6 col-xs-12">
+                    <div class="footer-widget about-widget">
+                        <!-- <a href="index.html"><img src="img/header_logo_cnc.jpg" alt="Awesome Image"/></a> -->
+                        <div id="contact_info" class="contact-info">
+                            <h3>Contact with us</h3>
+                            <div class="single-contact-info">
+                                <i class="zxp-icon-old-telephone-ringing"></i>
+                                <p>(+994) 50 247 24 84</p>
+                            </div><!-- /.single-contact-info -->
+                            <div class="single-contact-info">
+                                <i class="fas fa-envelope-open"></i>
+                                <p>office@azercnc.com</p>
+                            </div><!-- /.single-contact-info -->
+                            <div class="single-contact-info">
+                                <i class="fas fa-home"></i>
+                                <p>Dərnəgül şosesi</p>
+                            </div><!-- /.single-contact-info -->
+                        </div>
+                    </div><!-- /.footer-widget about-widget -->
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-5 col-sm-6 col-xs-12">
+                    
+                </div>
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.footer-top -->
+</footer><!-- /.site-footer -->
+<div class="footer-bottom">
+    <div class="container">
+        <div class="copy-text pull-left">
+            <p>2021 © All rights reserved</p>
+        </div><!-- /.copy-text -->
+        <div class="right-link pull-right">
+            <a href="#">Site by Greenlight Media Group</a>
+        </div><!-- /.right-link -->
+    </div><!-- /.container -->
+</div><!-- /.footer-bottom -->
+
+<div class="search_area zoom-anim-dialog mfp-hide" id="test-search">
+    <div class="search_box_inner">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search...">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button"><i class="fas fa-search"></i></button>
+            </span>
+        </div>
+    </div>
+</div>
+
+<div class="scroll-to-top scroll-to-target" data-target="html"><i class="fa fa-angle-up"></i></div> 
---
+++ includes/header_inner.php
@@ -0,0 +1,49 @@
+<header class="header header-home-three">
+    <div class="header-top">
+        <div class="container">
+            <div class="pull-left left-contact-info">
+                        <p><i class="fas fa-envelope-open"></i>office@azercnc.com</p>
+                   <!----><p><i class="zxp-icon-old-telephone-ringing"></i>(+994) 70 552 39 39</p>
+            </div><!-- /.pull-left -->
+            <div class="pull-right right-contact-info">
+				<p>
+                    <!-- <i class="fas fa-globe"></i> -->
+                    <span class="select-box">
+                        <!-- <select class="selectpicker" name="languages">
+                            <option value="1">EN</option>
+                        </select> -->
+                    </span><!-- /.select-box -->
+                </p>
+            </div><!-- /.pull-right -->
+        </div><!-- /.container -->
+    </div><!-- /.header-top -->
+    <nav class="navbar navbar-default header-navigation stricky">
+        <div class="container clearfix">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed zxp-icon-menu" data-toggle="collapse" data-target=".main-navigation" aria-expanded="false"> </button>
+                <a class="navbar-brand" href="#">
+                    <img src="img/header_logo_cnc.jpg" alt="Hayat Group Logo"/>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse main-navigation mainmenu " id="main-nav-bar">
+                
+                <ul class="nav navbar-nav navigation-box">
+                    <li class="current"> 
+                        <a href="index.php#minimal-bootstrap-carousel">Home</a> 
+                    </li>
+                    <li> <a href="index.php#about" style="color:#242C33;">About</a> </li>
+                    <li> 
+                        <a href="index.php#direction" style="color:#242C33;">Directions</a> 
+                    </li>
+                    <li> 
+                        <a href="index.php#projects" style="color:#242C33;">Our Projects</a> 
+                    </li>
+                    <li> <a href="contact.php" style="color:#242C33;">Contact</a> </li>
+                </ul>                
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+    </nav>   
+</header><!-- /.header -->
